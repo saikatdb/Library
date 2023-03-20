b49b0dcb31b9905135e93facb1010408d0bf3df6
Saikat Debnath <112309999+saikatdb@users.noreply.github>
2023-03-20 20:31:21 +0530

    Add a button for users to add book Added a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it has been read.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 
-function book(title, author, pages, read) { //constructor
+//object constructor
+function createBook(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
@@ -10,28 +11,154 @@ function book(title, author, pages, read) { //constructor
       this.read = 'not read yet'
     };
 }
-  const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', '295', 'no')
+const theHobbit = new createBook('The Hobbit', 'J.R.R. Tolkien', '295', 'no')
+const harryPotter = new createBook("Harry Potter and the Philosopher's Stone", "J. K. Rowling","325", "no")
 
+addBookToLibrary(theHobbit)
+addBookToLibrary(harryPotter)
+
+//function to show book info
 function addBookToLibrary(obj) {
     myLibrary.push(obj)
 }
 
-addBookToLibrary(theHobbit)
-console.log(myLibrary)
 
-let table = document.querySelector('table');
-let tbody = document.querySelector('tbody');
+const body = document.querySelector('body');
+const table = document.querySelector('table');
+const tbody = document.querySelector('tbody');
 
+//function to display book properties
 function displayBooks() {
+   // Clear any existing rows from the table
+   tbody.innerHTML = '';
+   
   for (let i = 0; i < myLibrary.length; i++) {
     let row = tbody.insertRow(i);
     for (key in myLibrary[i]) {
       let cell = row.insertCell();
       let text = document.createTextNode(myLibrary[i][key]);
       cell.appendChild(text);
-      console.log(myLibrary[i][key])
     }
   }
-}
+};
 
-displayBooks()
+displayBooks();
+
+const btn = document.querySelector('.add');
+btn.addEventListener('click', () => {
+  const form = document.createElement('form');
+  body.appendChild(form);
+
+ //create 'p' element
+ for (i = 1; i <= 5; i++) {
+  const variableName = 'p' + i;
+  const para = document.createElement('p');
+  para.id = variableName
+  form.appendChild(para);
+ };
+
+ //create an input element for title
+ const title = document.createElement('input');
+ title.setAttribute('type', 'text');
+ title.setAttribute('name', 'title');
+ title.setAttribute('required', 'true');
+ title.setAttribute('placeholder', 'TITLE');
+ //declaring 'p1' so that no bugs with scope happens
+ const p1 = document.getElementById('p1'); 
+ p1.appendChild(title);
+
+ //create an input element for author
+ const author = document.createElement('input');
+ author.setAttribute('type', 'text');
+ author.setAttribute('name', 'author');
+ author.setAttribute('required', 'true');
+ author.setAttribute('placeholder', 'AUTHOR');
+ //declaring 'p2' so that no bugs with scope happens
+ const p2 = document.getElementById('p2'); 
+ p2.appendChild(author);
+
+ //create an input element for pages
+ const pages = document.createElement('input');
+ pages.setAttribute('type', 'number');
+ pages.setAttribute('name', 'pages');
+ pages.setAttribute('required', 'true');
+ pages.setAttribute('placeholder', 'PAGES');
+ //declaring 'p3' so that no bugs with scope happens
+ const p3 = document.getElementById('p3'); 
+ p3.appendChild(pages);
+
+ //create an input element for reading status
+ const status = document.createElement('input');
+ status.setAttribute('type', 'checkbox');
+ status.setAttribute('name', 'status');
+ status.setAttribute('id', 'status');
+ //create a label element for status 
+ const statusLabel = document.createElement('label');
+ statusLabel.setAttribute('for', 'status');
+ statusLabel.textContent = 'Have you read it?';
+ //declaring 'p4' so that no bugs with scope happens
+ const p4 = document.getElementById('p4'); 
+ p4.appendChild(statusLabel);
+ p4.appendChild(status);
+
+ //create submit and cancel button
+ const submit = document.createElement('button');
+ const cancel = document.createElement('button');
+ submit.setAttribute('id', 'submit');
+ submit.setAttribute('type', 'submit');
+ cancel.setAttribute('id', 'cancel')
+ submit.textContent = 'SUBMIT';
+ cancel.textContent = 'CANCEL';
+ //declaring 'p5' so that no bugs with scope happens
+ const p5 = document.getElementById('p5');
+ p5.appendChild(cancel);
+ p5.appendChild(submit);
+
+ //disable button to prevent duplicate form
+ btn.disabled = true;
+
+ //submit button event
+ submit.addEventListener('click', function submitBook(e){
+  const titleName = title.value;
+  const authorName = author.value;
+  const pageNumber = pages.value;
+
+  if (titleName && authorName && pageNumber) {
+    if (status.checked) {
+      const obj = {};
+      const bookName = titleName;
+      obj[bookName] = new createBook (titleName, authorName, pageNumber, 'yes');
+      const book = obj[bookName];
+      console.log(book)
+      addBookToLibrary(book);
+      console.log(myLibrary)
+    } else if (!status.checked) {
+      const obj = {};
+      const bookName = titleName;
+      obj[bookName] = new createBook (titleName, authorName, pageNumber, 'no');
+      const book = obj[bookName];
+      addBookToLibrary(book);
+    }
+
+    displayBooks();
+    form.remove();
+    btn.disabled = false;
+    e.preventDefault();
+    
+  } else {
+    console.log('empty')
+    return;
+  }
+ })
+
+ //cancel button event
+ cancel.addEventListener('click', (e) => {
+  form.remove();
+  btn.disabled = false;
+  e.preventDefault();
+ })
+})
+
+
+
+console.log(myLibrary)
